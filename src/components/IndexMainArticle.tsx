d20abb94f052e4f96fb45781cb4cffd7bbc11b10
Ethan Zhou <ethanzhou76@gmail.com>
2022-10-15 18:02:01 -0400

    Styled front page articles

--- src/components/IndexMainArticle.tsx
+++ src/components/IndexMainArticle.tsx
@@ -1,21 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
-import cloudscraper from 'cloudscraper';
 import getImage from '../utils/getImage';
-import encode from '../utils/encode';
-
-const ArticleWrapper = styled.div`
-  display: flex;
-`;
 
 const ArticleTitleWrapper = styled.h3`
   font-family: Teko;
+  &::before {
+    content: '';
+    background-color: black;
+    height: 8px;
+    width: 80px;
+    display: block;
+    margin: 0 0 8px 30px;
+    transition: 0.3s ease-out;
+  }
+`;
+
+const ArticleWrapper = styled.div`
+  display: flex;
+  transition: 0.3s ease-out;
+  cursor: pointer;
+  &:hover {
+    color: rgb(0, 140, 117);
+    ${ArticleTitleWrapper} {
+      &::before {
+        background-color: rgb(0, 140, 117);
+      }
+    }
+    background-color: rgba(0, 140, 117, 0.1);
+  }
+`;
+
+const ArticleImage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const ImageFilter = styled.div`
+  position: relative;
+  width: 450px;
+`;
+
+const FilterDiv = styled.div`
+  background: rgba(0, 0, 0, 0.05);
+  width: 100%;
+  height: 253.188px;
+  z-index: 1;
+  transition: 0.3s ease-out;
 `;
 
 const ArticleCategory = styled.span``;
 
-const ArticleTitle = styled.span``;
+const ArticleTitle = styled.span`
+  margin: 0 30px;
+  font-size: 30px;
+`;
 
 const IndexMainArticle = ({ title, url, imageLink }) => {
   const [image, setImage] = React.useState();
@@ -28,7 +68,14 @@ const IndexMainArticle = ({ title, url, imageLink }) => {
   }, [imageLink]);
   return (
     <ArticleWrapper>
-      {image && <img src={image}></img>}
+      {image && (
+        <ImageFilter>
+          <ArticleImage>
+            <img style={{ width: '450px' }} src={image} />
+          </ArticleImage>
+          <FilterDiv />
+        </ImageFilter>
+      )}
       <ArticleTitleWrapper>
         <ArticleCategory></ArticleCategory>
         <ArticleTitle>{title}</ArticleTitle>
